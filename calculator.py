943db8cb586ed0bdb2456b08dfb7c95ea0e79436
jeremy <jeremybourgeois191@icloud.com>
2026-08-11 18:56:11 -0400

    small changes

--- calculator.py
+++ calculator.py
@@ -1,1 +1,7 @@
-print("hello world")
+from tkinter import *
+window=Tk()
+window.title("Calculatrice")
+window.geometry("350x450")
+window.iconbitmap("calculator.ico")
+
+window.mainloop() # ouvrir la fenetre
